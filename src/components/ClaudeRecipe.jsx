a6d7940a3ce8098fe52c8e91a4b33fdf8dd365a0
collins-kimotho <collinskimotho16@gmail.com>
2025-01-16 09:38:17 +0300

    responsive design

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -7,7 +7,7 @@ const ClaudeRecipe = (props) => {
   return (
     <section className="flex flex-col items-center justify-center m-4 text-sm max-w-screen-md overflow-hidden mx-auto w-full mx-auto" aria-live='polite'>
       <h2 className='font-bold text-lg mb-2'>The Hugsy Chef Recommends:</h2>
-      <ReactMarkdown className='prose p-2 overflow-hidden rounded-sm' remarkPlugins={[remarkGfm]}>
+      <ReactMarkdown className='prose lg:prose-xl p-2 overflow-hidden rounded-sm' remarkPlugins={[remarkGfm]}>
          {props.recipe}
       </ReactMarkdown>
       
